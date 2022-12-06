70d52b27afc186cc6e736851f0f819af0cff2c8e
Fábio Picoli <fabiopicolijr@gmail.com>
2022-12-06 13:31:07 -0300

    frog

--- frog-game/scoreboard.py
+++ frog-game/scoreboard.py
@@ -1,5 +1,12 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+
+    def game_over(self):
+        print("Game Over")
